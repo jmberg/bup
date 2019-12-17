28dd64e8753640f3938d7f2b2f007230aafc8eeb
Johannes Berg <johannes@sipsolutions.net>
2019-12-17 22:35:10 +0100

    git: change PackWriter() objcache_maker so None is default
    Change the way we use objcache_maker and don't use None as
    the function when none is desired, but rather use a lambda
    that returns None, and then let =None mean the default.
    
    This simplifies things as you can pass =None to have the
    default even a few levels down in functions, right now it
    simplifies the server, but the next patch will push that
    into the Repo.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/git.py
+++ lib/bup/git.py
@@ -766,7 +766,7 @@ def _make_objcache():
 
 class PackWriter:
     """Writes Git objects inside a pack file."""
-    def __init__(self, objcache_maker=_make_objcache, compression_level=1,
+    def __init__(self, objcache_maker=None, compression_level=1,
                  run_midx=True, on_pack_finish=None,
                  max_pack_size=None, max_pack_objects=None, repo_dir=None):
         self.closed = False
@@ -777,7 +777,7 @@ class PackWriter:
         self.outbytes = 0
         self.filename = None
         self.idx = None
-        self.objcache_maker = objcache_maker
+        self.objcache_maker = objcache_maker or _make_objcache
         self.objcache = None
         self.compression_level = compression_level
         self.run_midx=run_midx
@@ -861,7 +861,7 @@ class PackWriter:
         return sha
 
     def _require_objcache(self):
-        if self.objcache is None and self.objcache_maker:
+        if self.objcache is None:
             self.objcache = self.objcache_maker()
         if self.objcache is None:
             raise GitError(
