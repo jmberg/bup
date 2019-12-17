4b61c9ffeb0c81a23ecadc762bc2149f1839d66d
Johannes Berg <johannes@sipsolutions.net>
2019-12-17 23:38:39 +0100

    protocol: create new protocol module
    Create a new protocol module from the server module (basically
    just renaming it) and move a bunch of functions that don't really
    belong into the vfs module as well.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

---
+++ test/int/test_protocol.py
@@ -0,0 +1,17 @@
+
+from __future__ import absolute_import, print_function
+from io import BytesIO
+import sys
+
+from wvpytest import *
+
+from bup import vfs, protocol
+
+
+def test_item_read_write():
+    x = vfs.Root(meta=13)
+    stream = BytesIO()
+    protocol.write_item(stream, x)
+    print('stream:', repr(stream.getvalue()), stream.tell(), file=sys.stderr)
+    stream.seek(0)
+    wvpasseq(x, protocol.read_item(stream))
