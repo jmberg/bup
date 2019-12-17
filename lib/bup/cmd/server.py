9f8bbb6a6320eb06706ea9f50919d917bd85701e
Johannes Berg <johannes@sipsolutions.net>
2019-12-17 22:48:19 +0100

    server: remove AbstractServerBackend/GitServerBackend
    Now that we've pushed everything down into LocalRepo, we
    can finally get rid of the server backend and just use the
    LocalRepo class directly.
    
    Due to the previous patch with rev-list, we cannot actually
    have a RemoteRepo used by the server, but that's OK, we
    don't really have a use case for that.
    
    I haven't quite figured out the semantics of check_repo_or_die
    and so this makes a tiny new wrapper class to call it.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/cmd/server.py
+++ lib/bup/cmd/server.py
@@ -1,9 +1,10 @@
 
 from __future__ import absolute_import
 import sys
-from bup import options
+from bup import options, git
 from bup.io import byte_stream
-from bup.server import BupProtocolServer, GitServerBackend
+from bup.server import BupProtocolServer
+from bup.repo import LocalRepo
 from bup.helpers import (Conn, debug2)
 
 optspec = """
@@ -18,7 +19,12 @@ def main(argv):
 
     debug2('bup server: reading from stdin.\n')
 
+    class ServerRepo(LocalRepo):
+        def __init__(self, repo_dir):
+            git.check_repo_or_die(repo_dir)
+            LocalRepo.__init__(self, repo_dir)
+
     with BupProtocolServer(Conn(byte_stream(sys.stdin),
                                 byte_stream(sys.stdout)),
-                           GitServerBackend) as server:
+                           ServerRepo) as server:
         server.handle()
