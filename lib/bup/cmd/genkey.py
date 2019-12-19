00ec8917b47ad8f79e6bbd5deaa994989ebbcd88
Johannes Berg <johannes@sipsolutions.net>
2019-12-19 13:54:51 +0100

    bup: support encrypted repositories
    Now, with a lot of pieces in place, add support for encrypted
    repositories. The code contains more detailed documentation on
    the actual encryption design.
    
    An encrypted repository is a config repository, and the newly
    added 'bup genkey' command can be used to create keys (and a
    partial template for the configuration file.)
    
    Also add documentation for all this, including for the storage
    abstraction and file storage backend (here since it's previously
    not actually useful and refers to the encrypted repository.)
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

---
+++ lib/bup/cmd/genkey.py
@@ -0,0 +1,21 @@
+
+import sys
+
+def main(argv):
+    try:
+        import libnacl.public
+        import libnacl.secret
+    except ImportError:
+        print("libnacl not found, cannot generate keys")
+        sys.exit(2)
+
+    pair = libnacl.public.SecretKey()
+    box = libnacl.secret.SecretBox()
+
+    print('[bup]')
+    print('  type = Encrypted')
+    print('  storage = TBD')
+    print('  cachedir = TBD')
+    print('  repokey = ' + box.hex_sk().decode('ascii'))
+    print('  readkey = ' + pair.hex_sk().decode('ascii'))
+    print('  writekey = ' + pair.hex_pk().decode('ascii'))
