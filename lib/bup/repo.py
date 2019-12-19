ac69b33537476e772b91ec0922e46325722aaeea
Johannes Berg <johannes@sipsolutions.net>
2019-12-19 14:21:27 +0100

    repo: add make_repo() function to create RemoteRepo instances
    Make everything go through make_repo so that we can use it in
    the future to instantiate other kinds of "remote" repositories,
    not just the ones that the Client currently handles.
    
    Ideally, we'd make _everything_ go through here, even LocalRepo,
    but currently that can't be done for two reasons:
     1) LocalRepo() still heavily relies on git.repodir, i.e. global
        state
     2) the file:// protocol that would be natural is already taken
        for starting a local bup-server
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/repo.py
+++ lib/bup/repo.py
@@ -129,6 +129,9 @@ class LocalRepo:
             raise git.GitError('git rev-list returned error %d' % rv)
 
 
+def make_repo(address):
+    return RemoteRepo(address)
+
 class RemoteRepo:
     def __init__(self, address):
         self.closed = True # in case Client instantiation fails
