fccfc4b19d04a2c93afbd21210768c13b76968ed
Johannes Berg <johannes@sipsolutions.net>
2020-01-23 18:37:11 +0100

    repo: honour git compression level configuration
    Make bup honour the git compression level, using, in
    order of precedence:
     * level specified on command line (passed to the
       PackWriter instantiation)
     * git config pack.compression
     * git config core.compression
     * default to the previous default of 1
    
    Note that the git config values are taken from the *repository*
    configuration, so they come from the remote repository when the
    data is stored remotely. This differs from the handling of the
    pack.packSizeLimit option, which currently comes only from the
    *local* bup repo (typically ~/.bup/) even when a backup is made
    to a remote repository.
    
    That last bullet makes it compatible with old versions of
    bup, whereas git uses -1 by default.
    
    Also allow -1, which python's zlib module accepts, and might
    be configured per the git config documentation.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/repo/base.py
+++ lib/bup/repo/base.py
@@ -27,6 +27,14 @@ class BaseRepo(object):
                  max_pack_size=None, max_pack_objects=None):
         self.closed = False
         self._id = _repo_id(key)
+        if compression_level is None:
+            compression_level = self.config_get(b'pack.compression',
+                                                opttype='int')
+        if compression_level is None:
+            compression_level = self.config_get(b'core.compression',
+                                                opttype='int')
+        # if it's still None, use the built-in default in the
+        # lower levels (which should be 1)
         self.compression_level = compression_level
         self.max_pack_size = max_pack_size
         self.max_pack_objects = max_pack_objects
