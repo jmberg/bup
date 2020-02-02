0cf716f7cad48fd06c3d65dcf9fdad23d6a60f5d
Johannes Berg <johannes@sipsolutions.net>
2020-02-02 21:52:26 +0100

    server: use repo.config instead of git.git_config_get
    As I already introduced that earlier, it's a trivial replacement now.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/server.py
+++ lib/bup/server.py
@@ -411,6 +411,7 @@ class GitServerBackend(AbstractServerBackend):
         self.cat = self.repo.cat
         self.refs = self.repo.refs
         self.resolve = self.repo.resolve
+        self.config = self.repo.config
         self.list_indexes = self.repo.list_indexes
 
     def close(self):
@@ -442,6 +443,3 @@ class GitServerBackend(AbstractServerBackend):
         rv = p.wait()  # not fatal
         if rv:
             raise git.GitError('git rev-list returned error %d' % rv)
-
-    def config(self, key, opttype):
-        return git.git_config_get(key, opttype=opttype)
