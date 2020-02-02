ffd0276c7b57bc937a2235ba227153968860d3b3
Johannes Berg <johannes@sipsolutions.net>
2020-02-02 21:29:02 +0100

    bup: add config command
    Add a "config" sub-command to bup to allow reading configuration
    values, which can be useful for debugging especially remote bup
    scenarios (where the bup version on both sides matters, etc.)
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

---
+++ lib/bup/cmd/config.py
@@ -0,0 +1,29 @@
+
+from __future__ import absolute_import, print_function
+
+from bup import options, repo
+from bup.compat import argv_bytes
+
+optspec = """
+bup config [--type=<path,int,str,bool>] <name>
+--
+r,remote=  proto://hostname/path/to/repo of remote repository
+t,type=    what type to interpret the value as
+"""
+
+def main(argv):
+    o = options.Options(optspec)
+    (opt, flags, extra) = o.parse_bytes(argv[1:])
+
+    if len(extra) != 1:
+        o.fatal("must give exactly one name")
+
+    name = argv_bytes(extra[0])
+
+    r = repo.from_opts(opt)
+
+    if opt.type == 'str':
+        opt.type = None
+    print("%s = %r" % (name.decode('utf-8'), r.config_get(name, opttype=opt.type)))
+
+    r.close()
