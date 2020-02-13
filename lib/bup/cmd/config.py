892ea93a2ce7d521c394ea60d54db2bdfd1bd9d3
Johannes Berg <johannes@sipsolutions.net>
2020-02-13 22:52:15 +0100

    bup: unify repo options parsing
    There's a lot of duplicated code, add a single new entry
    point for repo option parsing and use it where applicable.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/cmd/config.py
+++ lib/bup/cmd/config.py
@@ -1,11 +1,8 @@
 
 from __future__ import absolute_import, print_function
 
-import sys
-
-from bup import options, git, repo, client
-from bup.compat import argv_bytes, environ
-from bup.helpers import log
+from bup import options, repo
+from bup.compat import argv_bytes
 
 optspec = """
 bup config [--type=<path,int,str,bool>] <name>
@@ -18,32 +15,15 @@ def main(argv):
     o = options.Options(optspec)
     (opt, flags, extra) = o.parse(argv[1:])
 
-    git.check_repo_or_die()
     if len(extra) != 1:
         o.fatal("must give exactly one name")
 
     name = argv_bytes(extra[0])
 
-    is_reverse = environ.get(b'BUP_SERVER_REVERSE')
-    if is_reverse and opt.remote:
-        o.fatal("don't use -r in reverse mode; it's automatic")
-
-    if opt.remote:
-        opt.remote = argv_bytes(opt.remote)
-
-    try:
-        if opt.remote:
-            the_repo = repo.make_repo(opt.remote)
-        elif is_reverse:
-            the_repo = repo.make_repo(b'reverse://%s' % is_reverse)
-        else:
-            the_repo = repo.LocalRepo()
-    except client.ClientError as e:
-        log('error: %s' % e)
-        sys.exit(1)
+    r = repo.from_opts(opt)
 
     if opt.type == 'str':
         opt.type = None
-    print("%s = %r" % (name.decode('utf-8'), the_repo.config(name, opttype=opt.type)))
+    print("%s = %r" % (name.decode('utf-8'), r.config(name, opttype=opt.type)))
 
-    the_repo.close()
+    r.close()
