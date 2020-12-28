2a8195bad9a3048cafb4e0fa9e53e6f719d24ea1
Johannes Berg <johannes@sipsolutions.net>
2020-12-28 22:30:58 +0100

    tag: use repo abstraction
    Use the repo abstraction for creating/removing tags/refs, and
    add the necessary commands to make it work over remote.
    
    This breaks the non-atomicity of bup tag, i.e. you could have
    previously used bup tag concurrently, have multiple succeed,
    and yet obviously only one wins. This is not quite the case
    now, concurrent updates of the same tag may fail, but that
    doesn't seem like a big deal (especially given that there's
    no concurrency analysis/promise at all.) It'd require special
    API even to the server to restore this, so don't bother.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/cmd/tag.py
+++ lib/bup/cmd/tag.py
@@ -6,6 +6,7 @@ from bup import git, options
 from bup.compat import argv_bytes
 from bup.helpers import debug1, log
 from bup.io import byte_stream, path_msg
+from bup.repo import from_opts
 
 
 # FIXME: review for safe writes.
@@ -15,6 +16,7 @@ bup tag
 bup tag [-f] <tag name> <commit>
 bup tag [-f] -d <tag name>
 --
+r,remote=   hostname:/path/to/repo of remote repository
 d,delete=   Delete a tag
 f,force     Overwrite existing tag, or ignore missing tag when deleting
 """
@@ -23,59 +25,59 @@ def main(argv):
     o = options.Options(optspec)
     opt, flags, extra = o.parse_bytes(argv[1:])
 
-    git.check_repo_or_die()
+    with from_opts(opt) as repo:
+        refs = { r[0]: r[1] for r in repo.refs() if r[0].startswith(b'refs/tags/') }
 
-    tags = [t for sublist in git.tags().values() for t in sublist]
+        if opt.delete:
+            # git.delete_ref() doesn't complain if a ref doesn't exist.  We
+            # could implement this verification but we'd need to read in the
+            # contents of the tag file and pass the hash, and we already know
+            # about the tag's existance via "tags".
+            tag_name = argv_bytes(opt.delete)
+            refname = b'refs/tags/' + tag_name
+            if not opt.force and refname not in refs:
+                log("error: tag '%s' doesn't exist\n" % path_msg(tag_name))
+                sys.exit(1)
+            repo.delete_ref(b'refs/tags/%s' % tag_name)
+            sys.exit(0)
 
-    if opt.delete:
-        # git.delete_ref() doesn't complain if a ref doesn't exist.  We
-        # could implement this verification but we'd need to read in the
-        # contents of the tag file and pass the hash, and we already know
-        # about the tag's existance via "tags".
-        tag_name = argv_bytes(opt.delete)
-        if not opt.force and tag_name not in tags:
-            log("error: tag '%s' doesn't exist\n" % path_msg(tag_name))
+        if not extra:
+            for t in refs:
+                sys.stdout.flush()
+                out = byte_stream(sys.stdout)
+                out.write(t[len(b'refs/tags/'):])
+                out.write(b'\n')
+            sys.exit(0)
+        elif len(extra) != 2:
+            o.fatal('expected commit ref and hash')
+
+        tag_name, commit = map(argv_bytes, extra[:2])
+        if not tag_name:
+            o.fatal("tag name must not be empty.")
+        debug1("args: tag name = %s; commit = %s\n"
+               % (path_msg(tag_name), commit.decode('ascii')))
+        refname = b'refs/tags/' + tag_name
+
+        if refname in refs and not opt.force:
+            log("bup: error: tag '%s' already exists\n" % path_msg(tag_name))
             sys.exit(1)
-        tag_file = b'refs/tags/%s' % tag_name
-        git.delete_ref(tag_file)
-        sys.exit(0)
 
-    if not extra:
-        for t in tags:
-            sys.stdout.flush()
-            out = byte_stream(sys.stdout)
-            out.write(t)
-            out.write(b'\n')
-        sys.exit(0)
-    elif len(extra) != 2:
-        o.fatal('expected commit ref and hash')
+        if tag_name.startswith(b'.'):
+            o.fatal("'%s' is not a valid tag name." % path_msg(tag_name))
 
-    tag_name, commit = map(argv_bytes, extra[:2])
-    if not tag_name:
-        o.fatal("tag name must not be empty.")
-    debug1("args: tag name = %s; commit = %s\n"
-           % (path_msg(tag_name), commit.decode('ascii')))
+        try:
+            hash = repo.rev_parse(commit)
+        except git.GitError as e:
+            log("bup: error: %s" % e)
+            sys.exit(2)
 
-    if tag_name in tags and not opt.force:
-        log("bup: error: tag '%s' already exists\n" % path_msg(tag_name))
-        sys.exit(1)
-
-    if tag_name.startswith(b'.'):
-        o.fatal("'%s' is not a valid tag name." % path_msg(tag_name))
-
-    try:
-        hash = git.rev_parse(commit)
-    except git.GitError as e:
-        log("bup: error: %s" % e)
-        sys.exit(2)
-
-    if not hash:
-        log("bup: error: commit %s not found.\n" % commit.decode('ascii'))
-        sys.exit(2)
-
-    with git.PackIdxList(git.repo(b'objects/pack')) as pL:
-        if not pL.exists(hash):
+        if not repo.exists(hash):
             log("bup: error: commit %s not found.\n" % commit.decode('ascii'))
             sys.exit(2)
 
-    git.update_ref(b'refs/tags/' + tag_name, hash, None, force=True)
+        try:
+            oldref = refs.get(refname, None)
+            repo.update_ref(refname, hash, oldref)
+        except git.GitError as e:
+            log("bup: error: could not create tag '%s': %s" % (path_msg(tag_name), e))
+            sys.exit(3)
