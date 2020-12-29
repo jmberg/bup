823900dc9fd6e370cc63aafa0609180c052b1b0e
Johannes Berg <johannes@sipsolutions.net>
2020-12-29 22:41:07 +0100

    git/path: move cachedir out of git.repo()
    Instead of using git.repo('index-cache/...'), add a new
    path.cachedir() function that takes just the '...' part
    as the argument, and is independent of having a git repo
    initialized.
    
    It currently defaults to the same location (and in fact
    this also introduces path.defaultrepo() to avoid circular
    module dependencies between bup.git and bup.path), but
    this will also make it possible to put this into another
    location in the future (if we consider this properly for
    automatic exclusion from indexing.)
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/client.py
+++ lib/bup/client.py
@@ -3,7 +3,7 @@ from binascii import hexlify, unhexlify
 import os, re, struct, sys, time, zlib
 import socket
 
-from bup import git, ssh, vfs, vint, protocol
+from bup import git, ssh, vfs, vint, protocol, path
 from bup.compat import pending_raise
 from bup.helpers import (Conn, atomically_replaced_file, chunkyreader, debug1,
                          debug2, linereader, lines_until_sentinel,
@@ -88,10 +88,9 @@ class Client:
             # but crashes instead when doing b'%s' % None.
             cachehost = b'None' if self.host is None else self.host
             cachedir = b'None' if self.dir is None else self.dir
-            self.cachedir = git.repo(b'index-cache/%s'
-                                     % re.sub(br'[^@\w]',
-                                              b'_',
-                                              b'%s:%s' % (cachehost, cachedir)))
+            self.cachedir = path.cachedir(re.sub(br'[^@\w]',
+                                                 b'_',
+                                                 b'%s:%s' % (cachehost, cachedir)))
             if self.protocol == b'bup-rev':
                 self.pout = os.fdopen(3, 'rb')
                 self.pin = os.fdopen(4, 'wb')
