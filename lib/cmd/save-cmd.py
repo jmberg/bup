157dbdc1e7ad484d89e05d45cc37e4f24e7fc1f7
Johannes Berg <johannes@sipsolutions.net>
2020-12-31 23:33:03 +0100

    save: fix symlink target race
    If the symlink target changes while save is running, we can end up
    reading one target and recording it in the metadata, but then
    recording a different one (with perhaps a different size) in the
    blob.
    
    Fix this by not reading the symlink again, but just using the one
    we already have in the metadata.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/cmd/save-cmd.py
+++ lib/cmd/save-cmd.py
@@ -464,13 +464,7 @@ for (transname,ent) in r.filter(extra, wantrecurse=wantrecurse_during):
         elif stat.S_ISDIR(ent.mode):
             assert(0)  # handled above
         elif stat.S_ISLNK(ent.mode):
-            try:
-                rl = os.readlink(ent.name)
-            except (OSError, IOError) as e:
-                add_error(e)
-                lastskip_name = ent.name
-            else:
-                (mode, id) = (GIT_MODE_SYMLINK, w.new_blob(rl))
+            (mode, id) = (GIT_MODE_SYMLINK, w.new_blob(meta.symlink_target))
         else:
             # Everything else should be fully described by its
             # metadata, so just record an empty blob, so the paths
