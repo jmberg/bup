195c239fc1622256d62fb3ecf81149bf5e651377
Johannes Berg <johannes@sipsolutions.net>
2024-10-22 18:49:56 +0200

    git: walk_object: fix item order
    There's a (potential) issue with 'bup get' in how it transfers objects
    due the the iteration order that we get from walk_object. Consider a
    tree fragment like this:
    
      ... ---- tree1 +---- blob1a
                     +---- blob1b
    
    Once it arrives at tree1, walk_object() will yield the objects in this
    order:
     - tree1
     - blob1a
     - blob1b
    
    If, due to prior objects transfered, 'blob1a' completes a pack file in
    the destination repository, that means the objects 'tree1' and 'blob1a'
    are permanently stored in the destination repository. However, if bup
    get is now somehow interrupted before the next file is completed, the
    destination repo still has 'tree1' and 'blob1a', although at this point
    they're not connected to anything.
    
    However, a further bup get that wants to transfer 'tree1', perhaps just
    the same command called again since the previous one was aborted, will
    now find that 'tree1' already exists and thus not recurse into it. Once
    the second transfer completes, the destination repo is broken, it has
    'tree1' connected somewhere, but is missing 'blob1b'.
    
    To fix this issue, change the tree walk order in walk_object() which is
    used by bup get so that we walk "leaves first", i.e.:
     - blob1a
     - blob1b
     - tree1
    
    (though the order of blobs doesn't matter.)
    
    This way, any interruptions anywhere will result in unconnected data as
    before, but will not result in tree objects being written with missing
    leaves. Resuming the interrupted bup get command will now work fine.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/git.py
+++ lib/bup/git.py
@@ -1448,11 +1448,18 @@ def walk_object(get_ref, oidx, stop_at=None, include_data=None):
     missing from the repository, and don't read or return blob content
     in the data field unless include_data is set.
 
+    Note that this must be leaves (dependencies) first, to avoid e.g.
+    interrupted bup get not being properly resumable, possibly leaving
+    the destination repository with missing objects if resumed.
     """
     # Maintain the pending stack on the heap to avoid stack overflow
     pending = [(oidx, [], [], None)]
     while len(pending):
-        oidx, parent_path, chunk_path, mode = pending.pop()
+        next_item = pending.pop()
+        if not isinstance(next_item, tuple):
+            yield next_item
+            continue
+        oidx, oid_path, parent_path, chunk_path, mode = next_item
         oid = unhexlify(oidx)
         if stop_at and stop_at(oidx):
             continue
@@ -1483,18 +1490,22 @@ def walk_object(get_ref, oidx, stop_at=None, include_data=None):
         else:
             data = b''.join(item_it)
 
-        yield WalkItem(oid=oid, type=typ,
+        itm = WalkItem(oid=oid, type=typ,
                        chunk_path=chunk_path, path=parent_path,
                        mode=mode,
                        data=(data if include_data else None))
 
-        if typ == b'commit':
+        if typ == b'blob':
+            yield itm
+        elif typ == b'commit':
+            pending.append(itm) # yield after the child(ren)
             commit_items = parse_commit(data)
             for pid in commit_items.parents:
                 pending.append((pid, parent_path, chunk_path, mode))
             pending.append((commit_items.tree, parent_path, chunk_path,
                             hashsplit.GIT_MODE_TREE))
         elif typ == b'tree':
+            pending.append(itm) # yield after the child(ren)
             for mode, name, ent_id in tree_decode(data):
                 demangled, bup_type = demangle_name(name, mode)
                 if chunk_path:
