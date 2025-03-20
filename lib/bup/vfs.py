91800a6b6c44c8c77c9f8270f6170c2479567dcc
Johannes Berg <johannes@sipsolutions.net>
2025-03-20 23:40:58 +0100

    vfs: also read old-style split trees
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/vfs.py
+++ lib/bup/vfs.py
@@ -664,15 +664,19 @@ def tree_items(repo, oid, tree_data, names, *, want_meta=True):
     depth = None
     bupm_oid = None
     for _, mangled_name, sub_oid in entries:
+        if mangled_name.endswith(b'.bupd'):
+            depth = int(mangled_name.split(b'.')[0].decode('ascii'))
+            if not dot_requested: # all other metadata in "leaf" .bupm files
+                break
         if mangled_name.startswith(b'.bupd.'):
             depth = _parse_tree_depth(mangled_name)
             if not dot_requested: # all other metadata in "leaf" .bupm files
                 break
         if mangled_name == b'.bupm':
             bupm_oid = sub_oid
-            break
-        if mangled_name > b'.bupm':
-            break
+#            break
+#        if mangled_name > b'.bupm':
+#            break
 
     if want_meta and bupm_oid:
         if depth is None:
