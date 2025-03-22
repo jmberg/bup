4d8e52a8af37961db896135a4d8e58300d1b8e18
Johannes Berg <johannes@sipsolutions.net>
2025-03-22 13:56:10 +0100

    repo: document update_ref() must finish_writing()
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/repo/base.py
+++ lib/bup/repo/base.py
@@ -179,6 +179,8 @@ class BaseRepo:
         didn't exist) to newval, atomically doing a check against oldval
         and updating to newval. Both oldval and newval are given as oidx
         (hex-encoded oid).
+        Must also finish_writing() internally, so that all objects are
+        committed before the ref is updated.
         """
 
     @notimplemented
