a0fcbb0b3f05361e2b392adbb77cc3fb9366fd56
Johannes Berg <johannes@sipsolutions.net>
2025-03-26 11:14:19 +0100

    vfs: don't replace (dir) metadata size None with 0
    If we don't know the size, don't pretend we do know
    it's 0. This likely really just affects the display
    in ls/web for directories right now, however, it'll
    start mattering more for 'bup rewrite' which would
    otherwise always fill in "0" for directories that
    should have "None" for their size (i.e. not stored.)
    
    Update the tests to check against None now and to
    compare dir size to "-" rather than "0" in ls.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/vfs.py
+++ lib/bup/vfs.py
@@ -364,8 +364,6 @@ def _read_dir_meta(bupm):
     if not m:
         return default_dir_mode
     assert m.mode is not None
-    if m.size is None:
-        m.size = 0
     return m
 
 def _treeish_tree_data(repo, oid):
@@ -432,6 +430,8 @@ def _compute_item_size(repo, item):
         if isinstance(item, FakeLink):
             return len(item.target)
         return len(_readlink(repo, item.oid))
+    if S_ISDIR(mode):
+        return None
     return 0
 
 def item_size(repo, item):
