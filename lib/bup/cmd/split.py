c84237c833f66b5f1ea3c51ba426db6c836a814c
Johannes Berg <johannes@sipsolutions.net>
2025-03-29 17:52:47 +0100

    hashsplit: Implement FastCDC
    Implement FastCDC per DOI 10.1109/TPDS.2023.3290770. I also
    considered the JC variant (DOI 10.1109/TPDS.2023.3290770)
    but have to work more on the implementation.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/cmd/split.py
+++ lib/bup/cmd/split.py
@@ -48,6 +48,8 @@ max-pack-objects=  maximum number of objects in a single pack
 fanout=    average number of blobs in a single tree
 bwlimit=   maximum bytes/sec to transmit to server
 #,compress=  set compression level to # (0-9, 9 is highest)
+mode=      hashsplit mode (overrides repo)
+bits=      hashsplit bits (overrides repo)
 """
 
 
@@ -253,7 +255,9 @@ def main(argv):
                 split_cfg = hashsplit.configuration(null_config_get)
         except ConfigError as ex:
             opt_parser.fatal(ex)
+        split_cfg['mode'] = opt.mode
         split_cfg['keep_boundaries'] = opt.keep_boundaries
+        if opt.bits: split_cfg['blobbits'] = int(opt.bits)
         if opt.name and writing:
             refname = opt.name and b'refs/heads/%s' % opt.name
             oldref = repo.read_ref(refname)
